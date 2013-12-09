bba26d330ddc8bd89c252652c762c44c18a36164
steve <stevedanko1@gmail.com>
2013-12-08 20:54:39 -0500

    added in tests for liability class

---
+++ liability_spec.rb
@@ -0,0 +1,43 @@
+require 'rspec'
+require 'pry'
+require_relative 'load_data'
+require_relative 'employee'
+require_relative 'tax_calculator'
+require_relative 'liability_class'
+
+describe Liability do
+
+  let(:employee1) { Employee.new({"annual_income" => "100", "tax_rate" => "5", "tax_paid" => "0"}) }
+  let(:employee2) { Employee.new({"annual_income" => "1000", "tax_rate" => "10", "tax_paid" => "50"}) }
+  let(:employee3) { Employee.new({"annual_income" => "10000", "tax_rate" => "10", "tax_paid" => "900"}) }
+  let(:employee4) { Employee.new({"annual_income" => "10000", "tax_rate" => "10", "tax_paid" => "900.75"}) }
+
+    it 'should calculate the amount of tax they owe' do
+      steve = TaxCalculator.new(employee1)
+      steve.calculate_tax
+      ben = steve.money_owed
+      expect(ben.amount_due).to eql(5.0)
+    end
+    
+    it 'should calculate the amount of tax they owe' do
+      steve = TaxCalculator.new(employee2)
+      steve.calculate_tax
+      ben = steve.money_owed
+      expect(ben.amount_due).to eql(50.0)
+    end
+
+    it 'should calculate the amount of tax they owe' do
+      steve = TaxCalculator.new(employee3)
+      steve.calculate_tax
+      ben = steve.money_owed
+      expect(ben.amount_due).to eql(100.0)
+    end
+
+    it 'should calculate the amount of tax they owe' do
+      steve = TaxCalculator.new(employee4)
+      steve.calculate_tax
+      ben = steve.money_owed
+      expect(ben.amount_due).to eql(99.25)
+    end
+
+end
--- tax_calculator_spec.rb
+++ tax_calculator_spec.rb
@@ -9,15 +9,29 @@ describe TaxCalculator do
 
   let(:employee1) { Employee.new({"annual_income" => "1000", "tax_rate" => "5", "tax_paid" => "0"}) }
   let(:employee2) { Employee.new({"annual_income" => "1000", "tax_rate" => "10", "tax_paid" => "500"}) }
+  let(:employee3) { Employee.new({"annual_income" => "10", "tax_rate" => "10", "tax_paid" => "3"}) }
 
-    it 'calculate the amount of taxes that an employee still owes' do
+    it 'calculate the amount of taxes that an employee owes' do
       steve = TaxCalculator.new(employee1)
       expect(steve.calculate_tax).to eql(50.0)
-      binding.pry
     end
     
-    it 'calculate the amount of taxes that an employee should get in return' do
+    it 'calculate the amount of taxes an employee owes' do
       steve = TaxCalculator.new(employee2)
-      expect(steve.calculate_tax).to eql(400.0)
+      expect(steve.calculate_tax).to eql(100.0)
     end
+
+    it "should calculate the amount of money a employee gets a refund" do
+      person = TaxCalculator.new(employee2)
+      person.calculate_tax
+      binding.pry
+      expect(person.refund_amount).to eql(400.0)
+    end
+
+    it "should calculate the amount of money a employee gets in a refund" do
+      person = TaxCalculator.new(employee3)
+      person.calculate_tax
+      expect(person.refund_amount).to eql(2.0)
+    end
+
 end
